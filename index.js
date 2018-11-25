07882944d8686402f49243483ad0d9905b2c61d7
Al Motion <slmkitani@gmail.com>
2018-11-25 21:39:41 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var awsIot = require("aws-iot-device-sdk");
+
+var device = awsIot.device({
+  keyPath: "credentials/your-private.pem.key",
+  certPath: "credentials/your-certificate.pem.crt",
+  caPath: "credentials/your-aws-iot-rootCA.crt",
+  clientId: "",
+  host: "",
+  region: ""
+});
+
+device.publish(
+  "TestTopic",
+  JSON.stringify({ test_data: "hello from nodejs client" })
+);
